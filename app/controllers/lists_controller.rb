938a9097afeca3ea9799b892b9f8e15aa7f124f0
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-17 16:05:55 -0500

    Changed the redirect page for create action for lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new
     end
