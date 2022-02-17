2d99e7c602bcfa1f394a77ede8c05ca2abc6e7db
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-17 12:22:03 -0500

    Seed real movies from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts 'Deleting db....'
+Movie.destroy_all
+
+puts 'Creating movies....'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+json = URI.open(url).read
+movies = JSON.parse(json)
+movies['results'].each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
