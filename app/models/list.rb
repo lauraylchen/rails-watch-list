08e025990012c49db9d7893261db089a524aeda6
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-18 11:39:54 -0500

    Added Active Storage to allow user to upload a photo for there list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
