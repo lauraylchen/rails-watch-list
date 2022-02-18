0b9b46bb719ca9888810556745928e5d6d79123e
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-18 18:16:13 -0500

    Added root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: "lists#index"
   # user can see all the lists
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
